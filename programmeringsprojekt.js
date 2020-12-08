bc6c41ecf17e8f16bac1a6beef66c2efc693fb6a
tildarosvall <71072551+tildarosvall@users.noreply.github.com>
2020-12-08 14:24:28 +0100

    Add files via upload

---
+++ programmeringsprojekt.js
@@ -0,0 +1,380 @@
+<script src="https://koda.nu/simple.js"> 
+
+  
+
+     var x1svan = 20; 
+
+    var y1svan = 270; 
+
+    var svan1Color = "white"; 
+
+       
+
+      var x2svan = 200; 
+
+    var y2svan = 260; 
+
+    var svan2Color = "white"; 
+
+     
+
+    var x3svan = 350; 
+
+    var y3svan = 290; 
+
+    var svan3Color = "white"; 
+
+
+	var svanRadie = 35;
+    
+
+  
+
+      var riktning = "höger"; 
+
+   
+
+      var xhimmel = 0; 
+
+      var yhimmel = 0;     
+
+      var himmelWidth = 700; 
+
+    var himmelHeight = 400; 
+
+      var himmelColor = "DodgerBlue"; 
+
+ 
+   
+
+      var xhav = 0; 
+
+    var yhav = 290; 
+
+      var havWidth = 700; 
+
+    var havHeight = 200; 
+
+    var havColor = "blue"; 
+    
+    
+     var xträdstam1 = 500; 
+
+    var yträdstam1 = 25; 
+
+      var trädstamWidth1 = 20; 
+
+    var trädstamHeight1 = 290; 
+
+    var trädstamColor = "brown"; 
+    
+   
+   var xträdstam2 = 300; 
+
+    var yträdstam2 = 40; 
+
+      var trädstamWidth2 = 20; 
+
+    var trädstamHeight2 = 290; 
+    
+	var trädstamColor = "brown"; 
+    
+    
+     var xträdstam3 = 100; 
+
+    var yträdstam3 = 30; 
+
+      var trädstamWidth3 = 20; 
+
+    var trädstamHeight3 = 290; 
+    
+	var trädstamColor = "brown"; 
+  
+   
+
+      var catX = 10; 
+
+      var catY = 230; 
+
+      var catSpeed = 2;  
+
+    
+
+    function update () 
+
+  { 
+
+      clearScreen(); 
+
+     
+
+  
+
+     rectangle(xhimmel,yhimmel,himmelWidth,himmelHeight, himmelColor); 
+
+  //Rita ut en blå rektangel som ska täcka mesta del av skärmen, den ska bli himmeln. X-koordinat= 0, Y-koordinat= 0, himmelWidth= 700, himmelHeight = 400. 
+
+   
+
+   
+
+       circle(x1svan,y1svan,svanRadie,svan1Color); 
+
+     
+
+      if (riktning == "höger") 
+
+    { 
+
+        x1svan += 1; 
+
+          y1svan += 0; 
+
+       
+
+    } 
+
+     
+
+  if (x1svan > 650) 
+
+       
+
+    { 
+
+       
+
+      riktning = "vänster"; 
+
+       
+
+    } 
+
+     
+
+  if (riktning == "vänster") 
+
+       
+
+    { 
+
+        x1svan -= 1; 
+
+          y1svan -= 0; 
+
+    } 
+
+     
+
+    if (x1svan <11) 
+
+         
+
+    { 
+
+         
+
+    riktning = "höger"; 
+
+         
+
+    } 
+
+     
+
+      
+
+//När x1 värdet kommer över 650, så studsar den tillbaka åt vänster. När x-värdet blir mindre än 11, så studsar den tillbaka åt höger.     
+
+         
+
+    circle(x2svan,y2svan,svanRadie,svan2Color); 
+
+    if (riktning == "höger") 
+
+    { 
+
+        x2svan += 1; 
+
+          y2svan += 0; 
+
+       
+
+    } 
+
+     
+
+  if (x2svan > 620) 
+
+       
+
+    { 
+
+       
+
+      riktning = "vänster"; 
+
+       
+
+    } 
+
+     
+
+  if (riktning == "vänster") 
+
+       
+
+    { 
+
+        x2svan -= 1; 
+
+          y2svan -= 0; 
+
+    } 
+
+     
+
+    if (x2svan <21) 
+
+         
+
+    { 
+
+         
+
+    riktning = "höger"; 
+
+         
+
+    } 
+
+     
+
+  
+
+//När x2 värdet kommer över 620, så studsar den tillbaka åt vänster. När x-värdet blir mindre än 21, så studsar den tillbaka åt höger. 
+
+       
+
+    circle(x3svan,y3svan,svanRadie,svan3Color);  
+
+    if (riktning == "höger") 
+
+    { 
+
+        x3svan += 1; 
+
+          y3svan += 0; 
+
+       
+
+    } 
+
+     
+
+  if (x3svan > 610) 
+
+       
+
+    { 
+
+       
+
+      riktning = "vänster"; 
+
+       
+
+    } 
+
+     
+
+  if (riktning == "vänster") 
+
+       
+
+    { 
+
+        x3svan -= 1; 
+
+          y3svan -= 0; 
+
+    } 
+
+     
+
+    if (x3svan <11) 
+
+         
+
+    { 
+
+         
+
+    riktning = "höger"; 
+
+         
+
+    } 
+
+     
+
+    
+
+// När x3 värdet kommer över 610, så studsar den tillbaka åt vänster. När x-värdet blir mindre än 11, så studsar den tillbaka åt höger. 
+
+    
+  
+
+    rectangle(xhav,yhav,havWidth,havHeight,havColor); 
+
+  //Rita ut en blå rektangel som ska täcka hela skärmen längst ner, den ska bli havet. X-koordinat= 0, Y-koordinat= 400, havWidth= 700, havHeight= 50. 
+  
+   rectangle(xträdstam1,yträdstam1,trädstamWidth1,trädstamHeight1,trädstamColor); 
+   rectangle(xträdstam2,yträdstam2,trädstamWidth2,trädstamHeight2,trädstamColor); 
+   rectangle(xträdstam3,yträdstam3,trädstamWidth3,trädstamHeight3,trädstamColor); 
+   
+
+  //Rita ut en brun rektangel som ska bli trädstammen för ett träd, höger om huset. X-koordinat= 500, Y-koordinat= 200, trädstamWidth= 20, trädstamHeight= 200. 
+
+   
+
+  
+
+     { 
+
+      
+
+    picture (catX,catY, "https://raw.githubusercontent.com/tildarosvall/webbutveckling/main/Sk%C3%A4rmavbild%202020-12-08%20kl.%2013.31.53.png"); 
+
+     
+
+    if (keyboard.d) 
+
+      catX += catSpeed; 
+
+    if (keyboard.a) 
+
+      catX -= catSpeed; 
+
+    if (keyboard.w) 
+
+      catY -= catSpeed; 
+
+    if (keyboard.s) 
+
+      catY += catSpeed; 
+        
+
+  
+
+    } 
+
+      
+
+  } 
+
+    
+
+   
+
+</script> 
